4ca5ec6e91d499ed6fcd2a2a22b1a1ead9e70062
Francis Conroy <francisc@posteo.net>
2022-10-07 12:53:54 +1100

    Add logic to update based on URL

--- main2.py
+++ main2.py
@@ -1,15 +1,19 @@
 import logging
+import re
 import sys
-import urllib
-import requests
 
+import requests
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+
+TRACK_URL_REGEX = r"https:\/\/open\.spotify\.com\/track\/(?P<track_id>[\d\w]*)\?si=[\d\w]*"
+URL_FMT_STRING = "spotify:track:{}"
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=sys.argv[1],
                                                client_secret=sys.argv[2],
                                                redirect_uri="http://localhost:9000",
                                                scope="playlist-modify-private"))
+
 
 # This version has a different working principle. Locates tracks that are local only then prompts for a TRACK URL to replace the track.
 # If no URL is given the option is provided to remove the track OR skip.
@@ -43,31 +47,38 @@ while results:
             print(f"track {totalidx} of {results['total']}")
             print_track_artist_information(pl_item["track"])
             name, artist = get_name_artist(pl_item["track"])
-            try:
-                ser_results = sp.search(f"track:{name} artist:{artist}", limit=3, market="AU")
-            except requests.exceptions.ReadTimeout:
-                print("Request timed out!")
-                continue
-            except requests.exceptions.HTTPError:
-                logging.exception("HTTP Error")
-                continue
-            except spotipy.exceptions.SpotifyException:
-                logging.exception("SpotifyException")
-                continue
-            tracks = ser_results.get("tracks", {}).get("items", [])
-            if tracks:
-                print("\nPOTENTIAL MATCHES FOUND!")
-                for idx, sr in enumerate(tracks):
-                    print(f"{idx}.")
-                    ser_name, ser_artist = get_name_artist(sr)
-                    print_track_artist_information(sr, 1)
-                try:
-                    intval = int(input("\nPlease select the track ID or [enter] to skip"))
-                except ValueError:
-                    continue
-                update_list.append((totalidx, tracks[intval]["uri"]))
+
+            newurl = input("\nPLEASE PROVIDE A SUITABLE REPLACEMENT URL:")
+            print("\nSEARCHING...")
+            matdat = re.match(TRACK_URL_REGEX, newurl)
+            if matdat is not None:
+                track_id = matdat.group("track_id")
             else:
-                print("\nNO MATCH FOUND!!!!")
+                action = input("Invalid URL given! (r)emove track or (s)kip:")
+                if action == 'r':
+                    update_list.append((totalidx, None))
+                elif action == 's':
+                    continue
+                else:
+                    logging.error("INVALID ACTION GIVEN!")
+            try:
+                ser_results = sp.track(track_id)
+            except Exception:
+                logging.exception("Exception getting track info!")
+                continue
+
+            print("\nMatch found!\n")
+            print_track_artist_information(ser_results)
+            update_list.append((totalidx, ser_results["uri"]))
+            # except requests.exceptions.ReadTimeout:
+            #     print("Request timed out!")
+            #     continue
+            # except requests.exceptions.HTTPError:
+            #     logging.exception("HTTP Error")
+            #     continue
+            # except spotipy.exceptions.SpotifyException:
+            #     logging.exception("SpotifyException")
+            #     continue
 
     if results['next']:
         results = sp.next(results)
@@ -80,6 +91,7 @@ if y in ['Y', 'y']:
         for tup in update_list:
             writefile.write(",".join([str(x) for x in tup]) + '\n')
     old_uids, new_uids = zip(*update_list)
+    new_uids = [x for x in new_uids if x is not None]  # remove the Nones
     retry = True
     while retry:
         try:
@@ -96,5 +108,7 @@ if y in ['Y', 'y']:
         else:
             break
 
-https: // open.spotify.com / track / 1
-Thv8uCYzyOFC7PME9J936?si = d827ced1f91240e0
+# https://open.spotify.com/track/1Thv8uCYzyOFC7PME9J936?si=835f99ddd37a463d
+
+# https: // open.spotify.com / track / 24hYr62Cz2iA3OOef8mykI?si=622cec1c676140cd
+# '24hYr62Cz2iA3OOef8mykI'
